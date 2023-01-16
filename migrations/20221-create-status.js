fb61688d6f3570b72f1b43ca3d0327742fac38b7
Ali Almanea <alialmanea@192.168.1.103>
2023-01-16 20:24:34 +0300

    1st day

--- migrations/20221-create-status.js
+++ migrations/20221-create-status.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('status', {
+    await queryInterface.createTable("status", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -21,14 +21,34 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
-    await queryInterface.bulkInsert("status",[
-      {name: "todo", createdAt: Sequelize.fn("now"), updatedAt: Sequelize.fn("now")},
-      {name: "in progress", createdAt: Sequelize.fn("now"), updatedAt: Sequelize.fn("now")},
-      {name: "done", createdAt: Sequelize.fn("now"), updatedAt: Sequelize.fn("now")},
-      {name: "canceled", createdAt: Sequelize.fn("now"), updatedAt: Sequelize.fn("now")},
-    ])
+    await queryInterface.bulkInsert("status", [
+      {
+        id: 1,
+        name: "todo",
+        createdAt: Sequelize.fn("now"),
+        updatedAt: Sequelize.fn("now")
+      },
+      {
+        id: 2,
+        name: "in progress",
+        createdAt: Sequelize.fn("now"),
+        updatedAt: Sequelize.fn("now")
+      },
+      {
+        id: 3,
+        name: "done",
+        createdAt: Sequelize.fn("now"),
+        updatedAt: Sequelize.fn("now")
+      },
+      {
+        id: 3,
+        name: "canceled",
+        createdAt: Sequelize.fn("now"),
+        updatedAt: Sequelize.fn("now")
+      }
+    ]);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('status');
+    await queryInterface.dropTable("status");
   }
 };
